aff884539ed66f05d87115fcd064f1cd34211155
manuelExtra <hello@mxtechsolutions.com>
2025-12-02 12:22:29 +0100

    implement(feature): blog components addup

--- app/(root)/blog-posts/page.tsx
+++ app/(root)/blog-posts/page.tsx
@@ -11,6 +11,20 @@ const BlogPosts = () => {
           layer2='Blog Posts'
         />
       </header>
+      <section className='section-container-padding-0 mt-2'>
+        <div className='overflow-x-auto rounded-none'>
+          <div className='relative overflow-x-auto'>
+            {/* <BlogPostList
+              customers={customers}
+              count={totalCustomers}
+              onClickNext={customersOnClickNext}
+              onClickPrev={customersOnClickPrev}
+              currentPage={customersCurrentPage}
+              loading={customersLoading}
+            /> */}
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
